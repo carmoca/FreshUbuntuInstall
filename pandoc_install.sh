95381d1a1e28cdc836e6e054782fd421b664fe75
carmoca <carmoca@gmail.com>
2014-11-06 14:12:24 -0500

    Added fvwm, htop, ranger

--- pandoc_install.sh
+++ pandoc_install.sh
@@ -2,5 +2,5 @@
 
 gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
-sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray xload rxvt xcompmgr imagemagick
+sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray xload rxvt xcompmgr imagemagick htop ranger 
 
