02b4bce82a8e3f86f994e573bc8d6ae4ebc10b5e
carmoca <carmoca@gmail.com>
2014-11-06 20:45:23 -0500

    Added .vimrc, updated apt-get list

--- freshUbuntuInstall.sh
+++ freshUbuntuInstall.sh
@@ -2,5 +2,7 @@
 
 gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
-sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray xload rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential 
+sudo update-alternatives --config editor
+
+sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential subversion git vim
 
