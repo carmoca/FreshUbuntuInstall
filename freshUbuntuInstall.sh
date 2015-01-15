676e64a02cf7a12835f1bf60303466707fadaaa1
carmoca <carmoca@gmail.com>
2015-01-15 16:31:44 -0500

    Added ia32-libs for 32bit support.
    This was added to ensure that Android Studio works as advertised.

--- freshUbuntuInstall.sh
+++ freshUbuntuInstall.sh
@@ -10,14 +10,17 @@ gsettings set com.canonical.desktop.interface scrollbar-mode normal
 # certain command line tools I prefer (htop, vim ranger)
 # build essentials (...and git and SVN)
 # gimp, imagemagick
-sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential subversion git vim curl
+sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential subversion git vim curl ia32-libs
+
 
 # Sets the default editor
 sudo update-alternatives --config editor
 
 echo "HISTFILESIZE=" >> ~/.bashrc
 echo "HISTSIZE=" >> ~/.bashrc
-
+echo "export JAVA_HOME=~/jdk1.8.0_25" >> ~/.bashrc
+echo "export SDK_HOME=~/android/android-sdk-linux" >> ~/.bashrc
+echo "export PATH=$PATH:$JAVA_HOME/bin:$SDK_HOME/tools:$SDK_HOME
 
 mkdir ~/android
 
