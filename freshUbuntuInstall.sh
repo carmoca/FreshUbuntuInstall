a1d97f75e47ccc3afc6e16de9a76993d3475850b
Carl C <carmoca@gmail.com>
2015-05-16 10:54:09 -0400

    Added new settings and programs to install.

--- freshUbuntuInstall.sh
+++ freshUbuntuInstall.sh
@@ -10,7 +10,7 @@ gsettings set com.canonical.desktop.interface scrollbar-mode normal
 # certain command line tools I prefer (htop, vim ranger)
 # build essentials (...and git and SVN)
 # gimp, imagemagick
-sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential subversion git vim curl rxvt-unicode
+sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential subversion git vim curl rxvt-unicode libglib2.0-0:i386
 
 
 # Sets the default editor
