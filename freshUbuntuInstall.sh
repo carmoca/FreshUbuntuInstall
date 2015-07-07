d6189c93a5302b5cf21a557511a371ac8121e15f
carmoca <carmoca@gmail.com>
2015-07-07 08:40:50 -0700

    Added the command to remove unity-lens-shopping.

--- freshUbuntuInstall.sh
+++ freshUbuntuInstall.sh
@@ -25,3 +25,5 @@ echo "export PATH=$PATH:$JAVA_HOME/bin:$SDK_HOME/tools:$SDK_HOME" >> ~/.bashrc
 
 mkdir ~/android
 
+sudo apt-get remove unity-lens-shopping
+gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
