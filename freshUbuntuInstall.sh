bd7d32a4ccf36dbc7f5c118ca88999808f230ef8
carmoca <carmoca@gmail.com>
2015-08-20 14:19:23 -0400

    Removed ibus-pinyin Added new ibus-libpinyin Broke out the large install script into functional portions to make it easier to remove sections users do not wish to install - they will only need to comment out the lines for the software suites they do not wish to use.

--- freshUbuntuInstall.sh
+++ freshUbuntuInstall.sh
@@ -10,8 +10,23 @@ gsettings set com.canonical.desktop.interface scrollbar-mode normal
 # certain command line tools I prefer (htop, vim ranger)
 # build essentials (...and git and SVN)
 # gimp, imagemagick
-sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full fvwm stalonetray rxvt xcompmgr imagemagick htop ranger gimp virtualbox build-essential subversion git vim curl rxvt-unicode libglib2.0-0:i386 libfontconfig1:i386 libXrender1:i386 libsm6:i386 libfreetype6:i386 libxext6:i386
+sudo apt-get install pandoc texlive-base texlive-binaries texlive-extra-utils texlive-font-utils texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-extra texlive-latex-extra-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pictures texlive-pictures-doc texlive-pstricks texlive-pstricks-doc texlive-xetex texlive-fonts-extra texlive-full
 
+# This installed components for FVWM
+# I removed xcompmgr because it was causing the background to be covered over
+#sudo apt-get install fvwm stalonetray rxvt xcompmgr imagemagick 
+sudo apt-get install fvwm stalonetray rxvt imagemagick 
+
+# These are some very handy tools to have supporting development and basic system navigation and configuration
+sudo apt-get install htop ranger gimp virtualbox build-essential subversion git vim curl rxvt-unicode
+
+# Extra 32bit installers - useful for where 32 bit development occurs
+sudo apt-get install libglib2.0-0:i386 libfontconfig1:i386 libXrender1:i386 libsm6:i386 libfreetype6:i386 libxext6:i386 
+
+# If ibus-daemon and ibus-setup gives you any crap see this excellent writeup:
+# www.pinyinjoe.com/faq/ubuntu-1404-chinese-ime-problems-faq.htm
+sudo apt-get remove ibus-pinyin
+sudo apt-get install ibus-libpinyin
 
 # Sets the default editor
 sudo update-alternatives --config editor
@@ -27,3 +42,6 @@ mkdir ~/android
 
 sudo apt-get remove unity-lens-shopping
 gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-amazon.scope', 'more_suggestions-u1ms.scope', 'more_suggestions-populartracks.scope', 'music-musicstore.scope', 'more_suggestions-ebay.scope', 'more_suggestions-ubuntushop.scope', 'more_suggestions-skimlinks.scope']"
+
+cd ~ /.vim/bundle && git clone https://github.com/scrooloose/syntastic.git
+mkdir -p ~/.vim/autoload ~/.vim/bundle && wget -O ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
